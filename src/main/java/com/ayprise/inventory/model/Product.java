6dacfc3ccf0aa9835e1ed2f9a86639afd6f76e2a
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-16 18:19:31 -0400

    Minor refactoring

--- src/main/java/com/ayprise/inventory/model/Product.java
+++ src/main/java/com/ayprise/inventory/model/Product.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 /**
  * Product data class
  *
- * @param name - of the Product
- * @param category - of the Product
- * @param description - for the Product
+ * @param name - Product's name
+ * @param category Product category
+ * @param description Product's description
  */
 @Builder
-public record Product(String name, String category, String description) {
+public record Product(String name,
+                      String category,
+                      String description) {
 }
