daa2ad8efb5a048fe3b1958b437cf77cf73595a3
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-17 14:11:15 -0400

    Addressed code review

--- src/main/java/com/ayprise/inventory/model/ProductController.java
+++ src/main/java/com/ayprise/inventory/model/ProductController.java
@@ -1,25 +1,22 @@
 package com.ayprise.inventory.model;
 
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class ProductController {
-
-    private Map<String, Product> products = new HashMap<>() {{
-        put("1", new Product("Airpods Pro 2",
-                "Electronics",
-                "Apple Headphones"));
-    }};
+    private final Map<String, Product> products = Map.of("1", new Product("Airpods Pro 2",
+        "Electronics",
+        "Apple Headphones"));
 
     @GetMapping("/inventory/product/{id}")
     public Product get(@PathVariable("id") String id) {
-        Product product = products.get(id);
+        var product = products.get(id);
         if (product == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return product;
     }
