51bff55997e1a4b574bb3fffb1b1ac24ca64cab4
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-17 14:23:09 -0400

    Specified JSON response

--- src/main/java/com/ayprise/inventory/controller/ProductController.java
+++ src/main/java/com/ayprise/inventory/controller/ProductController.java
@@ -1,9 +1,9 @@
 package com.ayprise.inventory.controller;
 
 
-
 import com.ayprise.inventory.model.Product;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,10 +12,10 @@ import java.util.Map;
 @RestController
 public class ProductController {
     private final Map<String, Product> products = Map.of("1", new Product("Airpods Pro 2",
-        "Electronics",
-        "Apple Headphones"));
+            "Electronics",
+            "Apple Headphones"));
 
-    @GetMapping("/inventory/product/{id}")
+    @GetMapping(name = "/inventory/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Product get(@PathVariable("id") String id) {
         var product = products.get(id);
         if (product == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
