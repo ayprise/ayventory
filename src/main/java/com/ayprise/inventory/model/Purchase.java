ec6151b03073f7556e56e699ff8bd2042456f301
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-18 13:52:12 -0400

    Refactoring and Test adjustments

--- src/main/java/com/ayprise/inventory/model/Purchase.java
+++ src/main/java/com/ayprise/inventory/model/Purchase.java
@@ -1,34 +1,33 @@
 package com.ayprise.inventory.model;
 
 import lombok.Builder;
+
 import java.time.LocalDate;
 import java.util.List;
 
 /**
  * Purchases data class
  *
- * @param vendorName name of the vendor
- * @param orderNumber purchase order number
+ * @param vendorName   name of the vendor
+ * @param orderNumber  purchase order number
  * @param purchaseDate time of the purchase
- * @param receipt list of items bought with purchase
+ * @param items        list of items bought with purchase
  */
 @Builder(setterPrefix = "with")
 public record Purchase(String vendorName,
                        String orderNumber,
                        LocalDate purchaseDate,
-                       List<PurchaseItem> receipt) {
+                       List<PurchaseItem> items) {
 
     public Purchase {
         // Ensure immutability of items in the purchase
-        receipt = List.copyOf(receipt);
+        items = List.copyOf(items);
     }
 
     public double getTotal() {
-        double total = 0;
-        for (PurchaseItem item : receipt) {
-            total += (item.price() + item.salesTax()) * item.quantity();
-        }
-
-        return total;
+        return items.stream()
+                .mapToDouble(
+                        item -> (item.price() + item.salesTax()) * item.quantity())
+                .sum();
     }
 }
