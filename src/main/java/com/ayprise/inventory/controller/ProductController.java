fdb9c1b6ceee2370f1a950abccf0d8deae0126f5
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-18 18:17:22 -0400

    Revised Product REST endpoint

--- src/main/java/com/ayprise/inventory/controller/ProductController.java
+++ src/main/java/com/ayprise/inventory/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ayprise.inventory.controller;
 
 
 import com.ayprise.inventory.model.Product;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,8 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/inventory/product/{id}")
+@Tag(name = "Product")
+@RequestMapping("/inventory/product/")
 public class ProductController {
     private final Map<String, Product> products = Map.of("1", new Product("Airpods Pro 2",
                     "Electronics",
@@ -19,7 +21,7 @@ public class ProductController {
                     "Electronics",
                     "Nintendo Switch 2 Mario Kart Bundle"));
 
-    @GetMapping(name = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Product get(@PathVariable("id") String id) {
         var product = products.get(id);
         if (product == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
