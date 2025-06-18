1b16171670c8d63fa993bbed9120756d39064c84
Rumbleyoshi <rumbleyoshi@gmail.com>
2025-06-18 11:19:45 -0400

    Added produce/consume attributes

--- src/main/java/com/ayprise/inventory/controller/PurchaseController.java
+++ src/main/java/com/ayprise/inventory/controller/PurchaseController.java
@@ -2,6 +2,7 @@ package com.ayprise.inventory.controller;
 
 import com.ayprise.inventory.model.Purchase;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +12,13 @@ import java.util.List;
 public class PurchaseController {
     private final List<Purchase> purchases = new ArrayList<>();
 
-    @PostMapping
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Purchase createPurchase(@RequestBody Purchase purchase) {
         purchases.add(purchase);
         return purchase;
     }
 
-    @GetMapping
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Purchase> getAllPurchases() {
         return purchases;
     }
